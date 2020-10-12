35446a974bb2e3591b4104e38f2fdbea6ef858ef
hchiam <hchiam@users.noreply.github.com>
2020-10-12 15:19:28 -0400

    working example

---
+++ README.md
@@ -0,0 +1,13 @@
+# Learning splay tree
+
+Just one of the things I'm learning. <https://github.com/hchiam/learning>
+
+<https://en.wikipedia.org/wiki/Splay_tree>
+
+<https://www.cs.usfca.edu/~galles/visualization/SplayTree.html>
+
+<https://github.com/slmoore/js-splay-tree/blob/master/splay-tree-clean.js>
+
+```bash
+node example.js
+```
---
+++ example.js
@@ -0,0 +1,162 @@
+function Node(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+Node.prototype.add = function (value) {
+  // assumes valid value
+  if (!this.value) {
+    this.value = value;
+    return;
+  }
+  if (value < this.value) {
+    if (this.left) {
+      this.left.add(value);
+    } else {
+      this.left = new Node(value);
+    }
+  } else {
+    // if value >= this.value:
+    if (this.right) {
+      this.right.add(value);
+    } else {
+      this.right = new Node(value);
+    }
+  }
+};
+
+function SplayBST() {
+  this.root = null;
+}
+
+SplayBST.prototype.add = function (value) {
+  // assumes valid value
+  if (!this.root) {
+    this.root = new Node(value);
+    return;
+  }
+  if (value < this.root.value) {
+    if (this.root.left) {
+      this.root.left.add(value);
+    } else {
+      this.root.left = new Node(value);
+    }
+  } else {
+    // if value >= this.root.value:
+    if (this.root.right) {
+      this.root.right.add(value);
+    } else {
+      this.root.right = new Node(value);
+    }
+  }
+  this.splay(value);
+};
+
+/**
+ * modified version of this:
+ * https://github.com/slmoore/js-splay-tree/blob/master/splay-tree-clean.js#L177
+ */
+SplayBST.prototype.splay = function (value) {
+  // returns new root
+  // assumes valid value
+  var splayRecursive = function (node, value) {
+    if (node === null) return null; // done
+
+    if (value < node.value) {
+      // look left:
+      if (node.left === null) return node; // done
+
+      if (value < node.left.value) {
+        // will need to (?) + zig zig:
+        node.left.left = splayRecursive(node.left.left, value);
+        node = this.rotateRight(node);
+      } else if (value > node.left.value) {
+        // will need to (?) + zag zig:
+        node.left.right = splayRecursive(node.left.right, value);
+        if (node.left.right !== null) node.left = this.rotateLeft(node.left);
+      }
+
+      if (node.left === null) {
+        return node; // done
+      } else {
+        return this.rotateRight(node); // zig
+      }
+    } else if (value > node.value) {
+      // look right:
+      if (node.right === null) return node; // done
+
+      if (value > node.right.value) {
+        // will need to (?) + zag zag:
+        node.right.right = splayRecursive(node.right.right, value);
+        node = this.rotateLeft(node);
+      } else if (value < node.right.value) {
+        // will need to (?) + zig zag:
+        node.right.left = splayRecursive(node.right.left, value);
+        if (node.right.left !== null) node.right = this.rotateRight(node.right);
+      }
+
+      if (node.right === null) {
+        return node; // done
+      } else {
+        return this.rotateLeft(node); // zag
+      }
+    } else {
+      // if value === node.value
+      return node; // done
+    }
+  }.bind(this);
+
+  this.root = splayRecursive(this.root, value);
+  return this.root;
+};
+
+SplayBST.prototype.rotateRight = function (node) {
+  // mnemonic for right rotation: left, child, rotate
+  var newRoot;
+  if (node) {
+    newRoot = node.left;
+    // move child around:
+    node.left = newRoot.right;
+    // actually do the rotation:
+    newRoot.right = node;
+  }
+  return newRoot;
+};
+
+SplayBST.prototype.rotateLeft = function (node) {
+  // mnemonic for left rotation: right, child, rotate
+  var newRoot;
+  if (node) {
+    newRoot = node.right;
+    // move child around:
+    node.right = newRoot.left;
+    // actually do the rotation:
+    newRoot.left = node;
+  }
+  return newRoot;
+};
+
+var splayTree = new SplayBST();
+splayTree.add(5);
+splayTree.add(1);
+splayTree.add(4);
+splayTree.add(2);
+splayTree.add(3);
+console.log(JSON.stringify(splayTree) + "\n\n");
+prettyPrintTree(splayTree.root);
+
+function prettyPrintTree(node, level = 0) {
+  if (!node || !node.value) return;
+  console.log(" ".repeat(level) + node.value);
+  if (node.left) {
+    prettyPrintTree(node.left, level + 1);
+  } else {
+    console.log(" ".repeat(level) + "x");
+  }
+  if (node.right) {
+    prettyPrintTree(node.right, level + 1);
+  } else {
+    console.log(" ".repeat(level) + "x");
+  }
+}
